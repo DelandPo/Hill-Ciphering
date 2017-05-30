28f56df9ec84cad7430c1cbdb7d763e47db03ac1
DelandPo <apoudel@stcloudstate.edu>
2017-05-29 23:29:39 -0500

    After going home

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -1,5 +1,10 @@
 import numpy as np
+keySize = int(input("Enter the size of the N X N Matrix for the Key :: "))
 a = [int(x) for x in input().split()]
+while(len(a) != keySize * keySize):
+    print("Please enter the key")
+    a = [int(x) for x in input().split()]
 y = np.array(a).reshape(2,2)
 print(np.linalg.det(y))
 print(np.linalg.inv(y))
+print(keySize)
