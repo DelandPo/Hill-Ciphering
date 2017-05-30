4814e72331342e9935339b4c3c67b1fb5a7edb64
DelandPo <apoudel@stcloudstate.edu>
2017-05-30 00:02:41 -0500

    Encryption is working

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -4,7 +4,12 @@ a = [int(x) for x in input().split()]
 while(len(a) != keySize * keySize):
     print("Please enter the key")
     a = [int(x) for x in input().split()]
-y = np.array(a).reshape(2,2)
+y = np.array(a).reshape(keySize,keySize)
+b = [int(x) for x in input().split()]
+plainText = np.array(b).reshape(len(b),1)
+finalResult = np.matmul(y,plainText)
+print(finalResult)
+print(y)
 print(np.linalg.det(y))
 print(np.linalg.inv(y))
-print(keySize)
+
