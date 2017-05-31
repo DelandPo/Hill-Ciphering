79214dea736efbddf80956abe513028dce1e7480
DelandPo <apoudel@stcloudstate.edu>
2017-05-30 19:53:10 -0500

    Working Hill Cipher

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -1,10 +1,7 @@
 import numpy as np
-
-
-
 def charArray(j):
     list1 = []
-    codelist = "abcdefghijklmnopqrstuvwxyz0123456789!\"#$%&\'()*"
+    codelist = "abcdefghijklmnopqrstuvwxyz"     # add it later on 0123456789!\"#$%&\'()*
     text = [q.lower() for q in j]
     try:
         for i in text:
@@ -16,6 +13,9 @@ def charArray(j):
         print("\nThat is not a valid input, try again. An Illegal Character Was Detected!\n")
         value = input("Press any key to return to the main menu...")
 
+l = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+r = [l.index(val) for val in l]
+d = dict(zip(r, l))
 keySize = int(input("Enter the size of the N X N Matrix for the Key :: "))
 a = input().split()
 while(len(a) != keySize * keySize):
@@ -27,13 +27,17 @@ b = input().split()
 if len(b)% keySize != 0 :
     for i in range(0,keySize - len(b)% keySize):
         b.append('#')
-b = charArray(b)
-plainText = np.array(b).reshape(keySize,len(b)//keySize)
+b = charArray(b[0:keySize])
+
+plainText = np.array(b).reshape(keySize,1)
 finalResult = np.matmul(y,plainText)
-print(plainText)
-print(y)
-print(finalResult)
-print(np.linalg.det(y))
-print(np.linalg.inv(y))
+encryptedString = ""
+
+for x in np.nditer(finalResult):
+    a = x%26
+    encryptedString += d[a]
+print(encryptedString)
+
+#def convertingString():
 
 
