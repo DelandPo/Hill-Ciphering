43a4627f1e43807f283001f252f879fcee493da3
DelandPo <apoudel@stcloudstate.edu>
2017-05-30 22:52:15 -0500

    Finally finished the ciphering part

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -1,4 +1,14 @@
 import numpy as np
+
+def convertingString(b,keySize,y):
+    encryptedString = ""
+    plainText = np.array(b).reshape(keySize,1)
+    finalResult = np.matmul(y,plainText)
+    for x in np.nditer(finalResult):
+        a = x%26
+        encryptedString += d[a]
+    return encryptedString
+
 def charArray(j):
     list1 = []
     codelist = "abcdefghijklmnopqrstuvwxyz"     # add it later on 0123456789!\"#$%&\'()*
@@ -17,6 +27,7 @@ l = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
 r = [l.index(val) for val in l]
 d = dict(zip(r, l))
 keySize = int(input("Enter the size of the N X N Matrix for the Key :: "))
+nValue = keySize
 a = input().split()
 while(len(a) != keySize * keySize):
     print("Please enter the valid key")
@@ -27,17 +38,15 @@ b = input().split()
 if len(b)% keySize != 0 :
     for i in range(0,keySize - len(b)% keySize):
         b.append('#')
-b = charArray(b[0:keySize])
+i = 0
+cipheredText = ""
+while keySize <= len(b):
+    c = charArray(b[i:keySize])
+    cipheredText = cipheredText + convertingString(c,nValue,y)
+    i = keySize
+    keySize = keySize + nValue 
+print(cipheredText)
+ 
 
-plainText = np.array(b).reshape(keySize,1)
-finalResult = np.matmul(y,plainText)
-encryptedString = ""
-
-for x in np.nditer(finalResult):
-    a = x%26
-    encryptedString += d[a]
-print(encryptedString)
-
-#def convertingString():
 
 
